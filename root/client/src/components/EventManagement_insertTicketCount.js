f54279c32fd65ad3dcd1ce196285c2bf8d7130ef
Oshan Gopallawa <oshan8998@gmail.com>
2024-05-09 10:49:42 +0530

    updating passing to finance
    updating passing to finance

--- root/client/src/components/EventManagement_insertTicketCount.js
+++ root/client/src/components/EventManagement_insertTicketCount.js
@@ -2,12 +2,20 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+function generateRandomId() {
+    const timestamp = new Date().getTime();
+    const randomNum = Math.floor(Math.random() * 10000); // Adjust as needed for desired length
+    return `E${timestamp}${randomNum}`;
+  }
+
 function InsertTicket() {
     const { Title, ticketCount } = useParams(); // Extract 'ticketCount' from URL parameters
     const navigate = useNavigate();
 
     // Convert 'ticketCount' to a number
     const Count = parseInt(ticketCount);
+
+    const randomId = generateRandomId();
 
     console.log("ticketCount:", ticketCount); // Add this line to log the value of ticketCount
 
@@ -20,7 +28,7 @@ function InsertTicket() {
                 };
                 await axios.put(`http://localhost:4000/event/update/${encodeURIComponent(Title)}/${encodeURIComponent(Count)}`, updatedEvent);
                 alert("Purchased");
-                navigate(`/Event/?pticket=${Title}`);//passing to finance
+                navigate(`/Event/?pticket=${Title}&id=${randomId}`);//passing to finance
             } catch (error) {
                 alert(error.message);
             }
