3c24d26d218574db395e9b1a95020e8dbea40e4b
P. Sehan Devinda <sehandevinda1@gmail.com>
2024-05-12 01:19:32 +0530

    Revert "Merge pull request #38 from SDP-1/supplier2"
    This reverts commit 30309cfcca7cd3a669dfe6499afcce342f9788f0, reversing
    changes made to 93889f0b13b4ef2e84a08006871dabaaf3d28387.

--- root/server/src/server.js
+++ root/server/src/server.js
@@ -87,20 +87,6 @@ app.use("/Rental", rentalRouter);
 http: receiptRouter = require("./routs/SalesManagement_receipt.js");
 app.use("/Receipt", receiptRouter);
 
-
-const supplierRouter = require("./routs/SupplierManagement_suppliers.js");
-app.use("/supplier",supplierRouter);
-
-const damageEquipmentSRouter = require('./routs/SupplierManagement_damageEquipmentR.js');
-app.use('/damageEquipment',damageEquipmentSRouter);
-
-const supplierEmailRouter = require('./routs/SupplierManagement_emailsupplierR.js');
-app.use('/supplierEmail', supplierEmailRouter);
-
-const equipmentSRouter = require('./routs/SupplierManagement_equipmentR.js');
-app.use('/equipment',equipmentSRouter);
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-module.exports = app;
