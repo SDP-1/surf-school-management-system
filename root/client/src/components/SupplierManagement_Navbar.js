769eda572221d133298bf1ab50941c0a2dbdea38
Charindu Liyanage <charinduliyanage17@gmail.com>
2024-05-12 01:02:19 +0530

    App.css changed
    extara changes are made

--- root/client/src/components/SupplierManagement_Navbar.js
+++ root/client/src/components/SupplierManagement_Navbar.js
@@ -1,78 +1,43 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
-const styles = {
-  dropdownToggle: {
-    color: 'white',      // Example: White color for text
-    fontWeight: 'bold'
-    // add your styles here
-  }
-};
+// const styles = {
+//   dropdownToggle: {
+//     color: 'white',      // Example: White color for text
+//     fontWeight: 'bold'
+//     // add your styles here
+//   }
+// };
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-primary">
+    <nav className="navbar navbar-expand-lg" style={{ background: "#1c4c74" }}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#">Supplier Management</Link>
+        <Link className="navbar-brand" to="#" style={{ color: "white" }}>Supplier Management</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/supplier/supplierDashboard">Dashboard</Link>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/supplier/supplierDashboard" style={{ color: "white" }}>Dashboard</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/supplier/allsup">Supplier</Link>
+              <Link className="nav-link active" aria-current="page" to="/supplier/allsup" style={{ color: "white" }}>Supplier</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/supplier/allequipment">Inventory</Link>
+              <Link className="nav-link active" aria-current="page" to="/supplier/allequipment" style={{ color: "white" }}>Inventory</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/supplier/damage">Damage Equipment</Link>
+              <Link className="nav-link active" aria-current="page" to="/supplier/damage" style={{ color: "white" }}>Damage Equipment</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/supplier/equipSup">Surf Board</Link>
+              <Link className="nav-link active" aria-current="page" to="/supplier/equipSup" style={{ color: "white" }}>Surf Board</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/supplier/websuits">Web Suits</Link>
+              <Link className="nav-link active" aria-current="page" to="/supplier/websuits" style={{ color: "white" }}>Web Suits</Link>
             </li>
-            {/* <li className="nav-item dropdown">
-        <a
-          className="nav-link dropdown-toggle"
-          href=""
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-          style={styles.dropdownToggle}
-        >
-          Equipment
-        </a>
-        <ul className="dropdown-menu">
-          <li><Link className="dropdown-item" to="/equip1">Equipment 1</Link></li>
-          <li><Link className="dropdown-item" to="/equip2">Equipment 2</Link></li>
-          <li><Link className="dropdown-item" to="/equip3">Equipment 3</Link></li>
-        </ul>
-      </li> */}
-      {/* <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Equipment
-              </a>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/equip1">Equipment 1</Link></li>
-                <li><Link className="dropdown-item" to="/equip2">Equipment 2</Link></li>
-                <li><Link className="dropdown-item" to="/equip3">Equipment 3</Link></li>
-              </ul>
-            </li> */}
-           
           </ul>
-          
         </div>
       </div>
     </nav>
